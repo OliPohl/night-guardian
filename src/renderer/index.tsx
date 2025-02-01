e27092e1bdf41e1f40e6fb0bc4791ac1269c8ec0
OliPohl <oliver@pohl66.de>
2025-02-01 15:28:58 +0100

    readability refactor for renderer

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,14 +1,20 @@
+// #region Imports
+// Importing necessary react libraries
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// Importing styles and components
 import './global.css'
-
 import Titlebar from './components/titlebar'
 import App from './components/app'
+// #endregion Imports
 
+
+// #region Render
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Titlebar />
     <App />
   </StrictMode>,
 )
+// #endregion Render
