5c015f1ecb37050e2455b5d3f1399ec1d870ec86
OliPohl <oliver@pohl66.de>
2025-02-01 15:22:31 +0100

    readability refactor of app

--- src/renderer/components/app/index.tsx
+++ src/renderer/components/app/index.tsx
@@ -1,15 +1,24 @@
+// #region Imports
+// Importing styles and components
 import './app.css';
 import Sidebar from '../sidebar';
 import Display from '../display';
+// #endregion Imports
 
+
+// #region App
 function App() {
+  // #region HTML
   return (
     <div id="app">
       <Display />
       <Sidebar />
+      {/* TODO: Remove the fm container */}
       <div id="form-modal-container" />
     </div>
   );
+  // #endregion HTML
 }
 
 export default App;
+// #endregion App
