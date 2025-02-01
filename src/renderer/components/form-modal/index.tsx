cb16cf98415da15013abbf35a2e104405c1e99da
OliPohl <oliver@pohl66.de>
2025-02-01 19:33:04 +0100

    dropdown and repeat button base functionality

--- src/renderer/components/form-modal/index.tsx
+++ src/renderer/components/form-modal/index.tsx
@@ -1,5 +1,6 @@
 // #region Imports
 // Importing necessary react libraries
+import { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // Importing styles and types
@@ -49,31 +50,129 @@ function FormModal(item: Guardian) {
       if (fmElement) {
         fmElement.innerHTML = '';
       }
-    };
+  };
 
-    // Saves the guardian and closes the form modal window
-    const saveGuardian = () => {
-      // TODO BACKEND: Save the guardian and display it to the frontend
+  // Saves the guardian and closes the form modal window
+  const saveGuardian = () => {
+    // TODO BACKEND: Save the guardian and display it to the frontend
+    closeWindow();
+  }
+
+  // Closes the form modal window when the background is clicked
+  const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
       closeWindow();
     }
-
-    // Closes the form modal window when the background is clicked
-    const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement>) => {
-      if (event.target === event.currentTarget) {
-        closeWindow();
-      }
-    };
+  };
   // #endregion Close/Save
 
 
+  // #region Repeats
+  const selectRepeatDay = (event: React.MouseEvent<HTMLImageElement>) => {
+    const isActive = event.currentTarget.classList.contains('fm-repeats-active');
+    if (isActive) {
+      event.currentTarget.classList.remove('fm-repeats-active');
+    } else {
+      event.currentTarget.classList.add('fm-repeats-active');
+    }
+  }
+
+
   // #region Dropdown
-  // TODO: Implement dropdown functionality
-  // TODO: Disable unrelevant dropdowns
+  // Closes all dropdowns
+  const closeAllDropdowns = () => { 
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.add('hidden');
+    }
+  }
+
+  // Opens the dropdown content when the dropdown is clicked and closes all other dropdowns
+  const openDropdownContent = (event: React.MouseEvent<HTMLDivElement>) => {
+    const dropdownContent = event.currentTarget.lastChild as HTMLDivElement;
+    const isHidden = dropdownContent.classList.contains('hidden');
+    closeAllDropdowns();
+    if (isHidden) {
+      dropdownContent.classList.remove('hidden');
+    }
+  }
+
+  // Closes all dropdowns when the user clicks outside of them
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!(event.target as HTMLElement).closest('.dropdown')) {
+        closeAllDropdowns();
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+
+  // Selects the dropdown content and closes the dropdown
+  const selectDropdownContent = (event: React.MouseEvent<HTMLDivElement>) => {
+    const newSelection = event.currentTarget.innerText;
+    const selectedItem = event.currentTarget.parentElement?.parentElement?.firstChild as HTMLElement;
+    if (selectedItem) {
+      selectedItem.innerText = newSelection;
+    }
+    disableImpossibleDropdowns();
+  }
+
+  // Disable all impossible dropdowns
+  const disableImpossibleDropdowns = () => {
+    const warningValue = document.getElementById('fm-warning-dropdown')?.firstChild?.textContent;
+
+    const snooze = document.getElementById('fm-snooze-dropdown') as HTMLElement;
+    const snoozeValue = document.getElementById('fm-snooze-dropdown')?.firstChild?.textContent;
+    // Disable snooze if warning is set to none
+    if (snooze) {
+      const snoozeText = snooze.firstChild as HTMLElement;
+      if (warningValue === 'None') {
+        snoozeText.textContent = 'None';
+        snooze.classList.add('dropdown-disabled');
+      } else {
+        snooze.classList.remove('dropdown-disabled');
+      }
+    }
+
+    const extension = document.getElementById('fm-extension-dropdown') as HTMLElement;
+    const extensionValue = extension?.firstChild?.textContent;
+    // Disable extension if snooze or warning is set to none
+    if (extension) {
+      const extensionText = extension.firstChild as HTMLElement;
+      if (snoozeValue === 'None' || warningValue === 'None') {
+        extensionText.textContent = 'None';
+        extension.classList.add('dropdown-disabled');
+      } else {
+        extension.classList.remove('dropdown-disabled');
+        if (extensionValue === 'None') {
+          extensionText.textContent = '5 min';
+        }
+      }
+    }
+
+    const equation = document.getElementById('fm-equation-dropdown') as HTMLElement;
+    // Disable equation if warning or snooze is set to none
+    if (equation) {
+      const equationText = equation.firstChild as HTMLElement;
+      if (snoozeValue === 'None' || warningValue === 'None') {
+        equationText.textContent = 'None';
+        equation.classList.add('dropdown-disabled');
+      } else {
+        equation.classList.remove('dropdown-disabled');
+      }
+    }
+  }
   // #endregion Dropdown
 
 
   // #region HTML
   return (
+    // TODO: use item to set the initial values of the form
     <div id="fm-background" className="form-modal" onClick={handleBackgroundClick}>
       <div id="fm-window" className="panel scrollable">
         {/* Heading */}
@@ -104,99 +203,99 @@ function FormModal(item: Guardian) {
 
         {/* Repeats */}
         <div id="fm-repeats">
-          <img src={imgRepeatMondays} alt="Monday" className=" " title="Repeats every Monday if active" />
-          <img src={imgRepeatTuesdays} alt="Tuesday" className="fm-repeats-active" title="Repeats every Tuesday if active" />
-          <img src={imgRepeatsWednesdays} alt="Wednesday" className="fm-repeats-active" title="Repeats every Wednesday if active" />
-          <img src={imgRepeatsThursdays} alt="Thursday" className="fm-repeats-active" title="Repeats every Thursday if active" />
-          <img src={imgRepeatsFridays} alt="Friday" className="fm-repeats-active" title="Repeats every Friday if active" />
-          <img src={imgRepeatsSaturdays} alt="Saturday" className="fm-repeats-active" title="Repeats every Saturday if active" />
-          <img src={imgRepeatsSundays} alt="Sunday" className="fm-repeats-active" title="Repeats every Sunday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatMondays} alt="Monday" className=" " title="Repeats every Monday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatTuesdays} alt="Tuesday" className="fm-repeats-active" title="Repeats every Tuesday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatsWednesdays} alt="Wednesday" className="fm-repeats-active" title="Repeats every Wednesday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatsThursdays} alt="Thursday" className="fm-repeats-active" title="Repeats every Thursday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatsFridays} alt="Friday" className="fm-repeats-active" title="Repeats every Friday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatsSaturdays} alt="Saturday" className="fm-repeats-active" title="Repeats every Saturday if active" />
+          <img onClick={selectRepeatDay} src={imgRepeatsSundays} alt="Sunday" className="fm-repeats-active" title="Repeats every Sunday if active" />
         </div>
 
         {/* Warning */}
         <div id="fm-warning" className="dropdown-wrapper" title="Time before the alarm when the guardian will prompt to snooze or acknowledge">
           <h2>Warning:</h2>
-          <div className="dropdown">
+          <div id="fm-warning-dropdown" className="dropdown" onClick={openDropdownContent}>
             <p>15 min</p>
             <div className="dropdown-flag">
               <img src={imgDropdownBtn} alt="Open" />
             </div>
             <div className="dropdown-content scrollable hidden">
-              <p>None</p>
-              <p>5 min</p>
-              <p>10 min</p>
-              <p>15 min</p>
-              <p>20 min</p>
-              <p>25 min</p>
-              <p>30 min</p>
-              <p>35 min</p>
-              <p>40 min</p>
-              <p>45 min</p>
-              <p>50 min</p>
-              <p>55 min</p>
-              <p>60 min</p>
+              <p onClick={selectDropdownContent}>None</p>
+              <p onClick={selectDropdownContent}>5 min</p>
+              <p onClick={selectDropdownContent}>10 min</p>
+              <p onClick={selectDropdownContent}>15 min</p>
+              <p onClick={selectDropdownContent}>20 min</p>
+              <p onClick={selectDropdownContent}>25 min</p>
+              <p onClick={selectDropdownContent}>30 min</p>
+              <p onClick={selectDropdownContent}>35 min</p>
+              <p onClick={selectDropdownContent}>40 min</p>
+              <p onClick={selectDropdownContent}>45 min</p>
+              <p onClick={selectDropdownContent}>50 min</p>
+              <p onClick={selectDropdownContent}>55 min</p>
+              <p onClick={selectDropdownContent}>60 min</p>
             </div>
           </div>
         </div>
 
         {/* Snooze */}
-        <div id="fm-snooze" className="dropdown-wrapper" title="Number of times the guardian can be delayed before forcing shutdown">
+        <div id="fm-snooze" className="dropdown-wrapper" title="Number of times the guardian can be delayed before forcing shutdown- requires warning time">
           <h2>Snooze:</h2>
-          <div className="dropdown">
+          <div id="fm-snooze-dropdown" className="dropdown" onClick={openDropdownContent}>
             <p>Unlimited</p>
             <div className="dropdown-flag">
               <img src={imgDropdownBtn} alt="Open" />
             </div>
             <div className="dropdown-content scrollable hidden">
-              <p>None</p>
-              <p>Once</p>
-              <p>Twice</p>
-              <p>3 Times</p>
-              <p>4 Times</p>
-              <p>5 Times</p>
-              <p>Unlimited</p>
+              <p onClick={selectDropdownContent}>None</p>
+              <p onClick={selectDropdownContent}>Once</p>
+              <p onClick={selectDropdownContent}>Twice</p>
+              <p onClick={selectDropdownContent}>3 Times</p>
+              <p onClick={selectDropdownContent}>4 Times</p>
+              <p onClick={selectDropdownContent}>5 Times</p>
+              <p onClick={selectDropdownContent}>Unlimited</p>
             </div>
           </div>
         </div>
 
         {/* Extension */}
-        <div id="fm-extension" className="dropdown-wrapper" title="Time added when snooze is selected - requires at least one snooze">
+        <div id="fm-extension" className="dropdown-wrapper" title="Time added when snooze is selected - requires at least one snooze or warning time">
           <h2>Extension:</h2>
-          <div className="dropdown dropdown-disabled">
-            <p>None</p>
+          <div id="fm-extension-dropdown" className="dropdown" onClick={openDropdownContent}>
+            <p>5 min</p>
             <div className="dropdown-flag">
               <img src={imgDropdownBtn} alt="Open" />
             </div>
             <div className="dropdown-content scrollable hidden">
-              <p>5 min</p>
-              <p>10 min</p>
-              <p>15 min</p>
-              <p>20 min</p>
-              <p>25 min</p>
-              <p>30 min</p>
-              <p>35 min</p>
-              <p>40 min</p>
-              <p>45 min</p>
-              <p>50 min</p>
-              <p>55 min</p>
-              <p>60 min</p>
+              <p onClick={selectDropdownContent}>5 min</p>
+              <p onClick={selectDropdownContent}>10 min</p>
+              <p onClick={selectDropdownContent}>15 min</p>
+              <p onClick={selectDropdownContent}>20 min</p>
+              <p onClick={selectDropdownContent}>25 min</p>
+              <p onClick={selectDropdownContent}>30 min</p>
+              <p onClick={selectDropdownContent}>35 min</p>
+              <p onClick={selectDropdownContent}>40 min</p>
+              <p onClick={selectDropdownContent}>45 min</p>
+              <p onClick={selectDropdownContent}>50 min</p>
+              <p onClick={selectDropdownContent}>55 min</p>
+              <p onClick={selectDropdownContent}>60 min</p>
             </div>
           </div>
         </div>
 
         {/* Equation */}
-        <div id="fm-equation" className="dropdown-wrapper" title="Difficulty of the equation to extend time - requires at least one snooze">
+        <div id="fm-equation" className="dropdown-wrapper" title="Difficulty of the equation to extend time - requires at least one snooze or warning time">
           <h2>Equation:</h2>
-          <div className="dropdown">
+          <div id="fm-equation-dropdown" className="dropdown" onClick={openDropdownContent}>
             <p>Easy</p>
             <div className="dropdown-flag">
               <img src={imgDropdownBtn} alt="Open" />
             </div>
             <div className="dropdown-content scrollable dropdown-content-top hidden">
-              <p>None</p>
-              <p>Easy</p>
-              <p>Medium</p>
-              <p>Hard</p>
+              <p onClick={selectDropdownContent}>None</p>
+              <p onClick={selectDropdownContent}>Easy</p>
+              <p onClick={selectDropdownContent}>Medium</p>
+              <p onClick={selectDropdownContent}>Hard</p>
             </div>
           </div>
         </div>
