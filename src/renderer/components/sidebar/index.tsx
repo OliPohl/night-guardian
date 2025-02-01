bddf76dfc2731b0944629673d51be3d9084f841b
OliPohl <oliver@pohl66.de>
2025-02-01 16:18:01 +0100

    readability refactor for sidebar

--- src/renderer/components/sidebar/index.tsx
+++ src/renderer/components/sidebar/index.tsx
@@ -1,27 +1,46 @@
+// #region Imports
+// Importing styles
 import './sidebar.css';
 
-import logo from './resources/logo.png';
-import githubMark from './resources/github-mark.svg';
+// Importing resources
+import imgLogo from './resources/img-logo.png';
+import imgGithubIcon from './resources/img-github-icon.svg';
+// #endregion Imports
 
+
+// #region Sidebar
 function Sidebar() {
+  // #region HTML
   return (
     <div id="sidebar">
-      <img id="logo" src={logo} alt="logo" />
+      {/* Logo */}
+      <img id="logo" src={imgLogo} alt="logo" />
+
+      {/* Description */}
       <div id="description" className="panel scrollable">
         <p>Night Guardian is your personal sleep asistant that helps promote healthy sleep habits.</p>
         <div className="small-divider"></div>
+
+        {/* Version / Github */}
         <div className="row">
           <p className="info-text">v-2.0</p>
           <div className="button" onClick={() => window.api.openGitHub()}>
-            <img src={githubMark} alt="github" />
+            <img src={imgGithubIcon} />
             <p>GitHub Repository</p>
           </div>
         </div>
         <div className="small-divider"></div>
+
+        {/* Tutorial */}
+        {/* TODO: tutorial button to video popup */}
+
+        {/* Credits */}
         <p className="info-text">made by OliPohl</p>
       </div>
     </div>
   );
+  // #endregion HTML
 }
 
 export default Sidebar;
+// #endregion Sidebar
