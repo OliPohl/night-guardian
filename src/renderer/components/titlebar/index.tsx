f9fa8d34620d70087595653ec0b89698d888654b
OliPohl <oliver@pohl66.de>
2025-02-01 16:25:16 +0100

    readability refactor for titlebar

--- src/renderer/components/titlebar/index.tsx
+++ src/renderer/components/titlebar/index.tsx
@@ -1,48 +1,71 @@
+// #region Imports
+// Importing necessary react libraries
 import { FC, useEffect } from 'react';
+
+// Importing styles
 import "./Titlebar.css";
-import minimizeIcon from './resources/minimize.svg'
-import maximizeIcon from './resources/maximize.svg'
-import unmaximizeIcon from './resources/unmaximize.svg'
-import closeIcon from './resources/close.svg'
 
+// Importing resources
+import imgMinimize from './resources/img-minimize.svg'
+import imgMaximize from './resources/img-maximize.svg'
+import imgUnmaximize from './resources/img-unmaximize.svg'
+import imgclose from './resources/img-close.svg'
+// #endregion Imports
+
+
+// #region Titlebar
 const Titlebar: FC = () => {
-  useEffect(() => {
-    // Changes the maximize icon when the window is maximized or unmaximized
-    const handleMaximize = (isMaximized: boolean) => {
-      if (isMaximized) {
-        document.getElementById('titlebar-maximize-img')?.classList.add('hidden');
-        document.getElementById('titlebar-unmaximize-img')?.classList.remove('hidden');
-      } else {
-        document.getElementById('titlebar-maximize-img')?.classList.remove('hidden');
-        document.getElementById('titlebar-unmaximize-img')?.classList.add('hidden');
-      }
-    };
+  // #region Maximize
+  // Changes the maximize icon when the window is maximized or unmaximized
+  const handleMaximize = (isMaximized: boolean) => {
+    if (isMaximized) {
+      document.getElementById('titlebar-maximize-img')?.classList.add('hidden');
+      document.getElementById('titlebar-unmaximize-img')?.classList.remove('hidden');
+    } else {
+      document.getElementById('titlebar-maximize-img')?.classList.remove('hidden');
+      document.getElementById('titlebar-unmaximize-img')?.classList.add('hidden');
+    }
+  };
 
-    // Listen for the maximize event
+  // Listen for the maximize event and call handleMaximize to change the icon
+  useEffect(() => {
     window.api.onMaximize(handleMaximize);
     return () => {
       window.api.onMaximize(() => {});
     };
   }, []);
+  // #endregion Maximize
 
 
+  // #region HTML
   return (
     <div className="titlebar">
+      {/* Heading */}
       <h1 className="titlebar-heading">Night Guardian</h1>
+      
+      {/* Buttons */}
       <div className="titlebar-buttons">
+        {/* Minimize */}
         <div id="titlebar-minimize" className="titlebar-button" onClick={() => window.api.minimizeWindow()}>
-          <img src={minimizeIcon} alt="Minimize" />
+          <img src={imgMinimize} alt="Minimize" />
         </div>
+        
+        {/* Maximize / Unmaximize */}
         <div id="titlebar-maximize" className="titlebar-button" onClick={() => window.api.maximizeWindow()}>
-          <img id="titlebar-maximize-img" src={maximizeIcon} alt="Maximize" />
-          <img id="titlebar-unmaximize-img" src={unmaximizeIcon} alt="Unmaximize" className="hidden" />
+          <img id="titlebar-maximize-img" src={imgMaximize} alt="Maximize" />
+          <img id="titlebar-unmaximize-img" src={imgUnmaximize} alt="Unmaximize" className="hidden" />
         </div>
+
+        {/* Close */}
         <div id="titlebar-close" className="titlebar-button" onClick={() => window.api.closeWindow()}>
-          <img src={closeIcon} alt="Close" />
+          <img src={imgclose} alt="Close" />
         </div>
+
       </div>
     </div>
   );
+  // #endregion HTML
 };
 
 export default Titlebar;
+// #endregion Titlebar
